f2ec73861628fc24c486e31643db32f042ad9281
prasantamohantydev <prasantamohanty2310@gmail.com>
2025-07-25 15:18:13 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="prasanta@google.com"
+var accountPassword="12345"
+accountCity="Balasore"
+let accountState
+
+// accountId=2 not allowed
+
+accountEmail="pm@google.com"
+accountPassword="54321"
+accountCity="Bhubaneswar"
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+console.log(accountId);
+
