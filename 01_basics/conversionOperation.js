1b4d9563a34676e801df4b8e7da57b8a2263ac85
prasantamohantydev <prasantamohanty2310@gmail.com>
2025-07-26 11:11:07 +0530

    Create conversionOperation.js

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score=""
+
+console.log(typeof score)
+
+let num=Number(score)
+console.log(typeof num);
+console.log(num);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=> true ; 
+//0=> false;
+//""=> false;
+//"prasanta"=>true;
+
+let someNumber=33
+let stringNumber=String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
