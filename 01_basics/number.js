a52d061689d79de3e5623413fe40e6426bad1e1d
prasantamohantydev <prasantamohanty2310@gmail.com>
2025-07-27 17:38:33 +0530

    Create number.js

---
+++ 01_basics/number.js
@@ -0,0 +1,39 @@
+// console.log(5);
+// let balance=new Number(123302.3944);
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(7));
+
+// let n=1000000;
+// console.log(n.toLocaleString());
+
+// //we can convert a number into sting an oporate the string operation
+// let s=(n.toString());
+// console.log(s.length)
+
+
+//-----------------------------MATHS--------------------------------
+
+console.log(Math);
+console.log(Math.abs(4));
+console.log(Math.pow(4,2));
+console.log(Math.round(4.794));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.min(1,3,6,2,-1));
+console.log(Math.max(1,3,6,2,-1));
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*10)+1))
+
+let min=10;
+let max=100;
+
+console.log(Math.floor((Math.random()*(max-min+1))+min))
+
+
