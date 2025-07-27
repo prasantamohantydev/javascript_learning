308934a6b4c2d1c042486f21ae29ff2006eef5f7
prasantamohantydev <prasantamohanty2310@gmail.com>
2025-07-27 19:27:43 +0530

    Create datesAndTime.js

---
+++ 01_basics/datesAndTime.js
@@ -0,0 +1,15 @@
+// let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+let date=new Date(2025 ,6,28)
+console.log(date.toString())
+
+console.log(Math.floor(Date.now()/1000)) 
+//It represents seconds since the midnight at the beginning of January 1, 1970, UTC (the epoch).
