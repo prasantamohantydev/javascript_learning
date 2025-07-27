cdf5544b4db8b589e61d9fb26d4cbbf68af64048
prasantamohantydev <prasantamohanty2310@gmail.com>
2025-07-27 16:33:06 +0530

    Create string.js

---
+++ 01_basics/string.js
@@ -0,0 +1,26 @@
+// const Name="Prasanta"
+// const repo=5
+// console.log("my name is "+Name+" and my repository count is "+repo);
+// console.log(`my name is ${Name} and my repository count is ${repo}`);
+
+//declare string through an object
+const name=new String('Prasanta Mohanty')
+
+console.log(name);
+console.log(name.length);
+console.log(name[0])
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+console.log(name.indexOf('a'));
+console.log(name.substring(0,4));
+console.log(name.slice(-14,4));
+console.log(name.trim());//it trims whitespaces
+console.log(name.replace('san','n'));
+console.log(name.includes('pinu'));
+console.log(name.split(''));
+
+
+
+
+
+
